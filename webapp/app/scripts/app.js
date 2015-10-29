6e9a1b3918984a06d78e7f2beabc18fb29090792
Ivan Micai da Silva <ivan@piertech.com.br>
2015-10-29 13:33:53 -0200

    webapp: Initial Commit (Yeomand Generator)

---
+++ webapp/app/scripts/app.js
@@ -0,0 +1,35 @@
+'use strict';
+
+/**
+ * @ngdoc overview
+ * @name lolStatsApp
+ * @description
+ * # lolStatsApp
+ *
+ * Main module of the application.
+ */
+angular
+  .module('lolStatsApp', [
+    'ngAnimate',
+    'ngCookies',
+    'ngResource',
+    'ngRoute',
+    'ngSanitize',
+    'ngTouch'
+  ])
+  .config(function ($routeProvider) {
+    $routeProvider
+      .when('/', {
+        templateUrl: 'views/main.html',
+        controller: 'MainCtrl',
+        controllerAs: 'main'
+      })
+      .when('/about', {
+        templateUrl: 'views/about.html',
+        controller: 'AboutCtrl',
+        controllerAs: 'about'
+      })
+      .otherwise({
+        redirectTo: '/'
+      });
+  });
---
+++ webapp/app/scripts/controllers/main.js
@@ -0,0 +1,17 @@
+'use strict';
+
+/**
+ * @ngdoc function
+ * @name lolStatsApp.controller:MainCtrl
+ * @description
+ * # MainCtrl
+ * Controller of the lolStatsApp
+ */
+angular.module('lolStatsApp')
+  .controller('MainCtrl', function () {
+    this.awesomeThings = [
+      'HTML5 Boilerplate',
+      'AngularJS',
+      'Karma'
+    ];
+  });
